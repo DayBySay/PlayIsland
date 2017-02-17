291e7b3b35889e632cc999ce623230f2fbc2c85e
t1609u06 <t1609u06@tky.shcool.dhw.co.jp>
2017-02-17 22:01:34 +0900

    add script for apple

---
+++ Assets/Scripts/Apple.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Apple : MonoBehaviour {
+	public enum STYLE {
+		RED,
+		BLUE,
+		GOLDEN,
+	}
+
+	[SerializeField]
+	private int point;
+	[SerializeField]
+	private STYLE style;
+
+	public delegate void DoActionDelegate();
+	public DoActionDelegate doAction;
+
+	void OnCollisionEnter(Collision collision) {
+		if (doAction == null) {
+			return;
+		}
+
+		doAction ();
+	}
