3b06e21de05c66d8dece93bb44d965432b7a68cb
Takayuki Sei <takayuki_sei@voyagegroup.com>
2017-02-18 23:09:10 +0900

    リンゴをインスタンス化するメソッド追加

--- Assets/Scripts/AppleService.cs
+++ Assets/Scripts/AppleService.cs
@@ -3,9 +3,43 @@ using System.Collections;
 
 public class AppleService : MonoBehaviour {
     [SerializeField]
+    private GameObject blueApple, redApple, goldApple;
+
+    public GameObject AppleWithStyle(Apple.STYLE style)
+    {
+        GameObject apple = null;
+        switch (style)
+        {
+            case Apple.STYLE.RED:
+                apple = redApple;
+                break;
+            case Apple.STYLE.BLUE:
+                apple = blueApple;
+                break;
+            case Apple.STYLE.GOLDEN:
+                apple = goldApple;
+                break;
+        }
+
+        return apple;
+    }
+    [SerializeField]
     private Transform[] applePivots;
 
-	public void CreateApple(Apple.STYLE style) {
-		
+    public enum ApplePivot
+    {
+        center,
+        left1,
+        left2,
+        left3,
+        right1,
+        right2,
+        right3,
+    }
+
+	public void CreateApple(Apple.STYLE style, ApplePivot pivot) {
+        GameObject apple = AppleWithStyle(style);
+        Instantiate(apple);
+        apple.transform.position = applePivots[(int)pivot].transform.position;
 	}
 }
