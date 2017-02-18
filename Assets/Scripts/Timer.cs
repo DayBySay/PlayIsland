55c4beb0128e4613306c1ed1d28db045506211db
Takayuki Sei <takayuki_sei@voyagegroup.com>
2017-02-19 00:29:58 +0900

    タイマーの実装追加

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class Timer : MonoBehaviour {
+    [SerializeField]
+    private int limit = 60;
+
+    private Subject<int> timerSubject = new Subject<int>();
+    public IObservable<int> OnTimeChanged;
+
+    private void Start()
+    {
+        StartCoroutine(TimerContinue());
+    }
+
+    IEnumerator TimerContinue()
+    {
+        int time = limit;
+        while (time > 0)
+        {
+            time--;
+            timerSubject.OnNext(time);
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
