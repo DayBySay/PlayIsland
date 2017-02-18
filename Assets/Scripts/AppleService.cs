f1888c1955762331e6cab14ab1482d7ba92f433c
Takayuki Sei <takayuki_sei@voyagegroup.com>
2017-02-18 22:54:53 +0900

    リンゴが落ちる場所を設定

--- Assets/Scripts/AppleService.cs
+++ Assets/Scripts/AppleService.cs
@@ -2,6 +2,9 @@
 using System.Collections;
 
 public class AppleService : MonoBehaviour {
+    [SerializeField]
+    private Transform[] applePivots;
+
 	public void CreateApple(Apple.STYLE style) {
 		
 	}
