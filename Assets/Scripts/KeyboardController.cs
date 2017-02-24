0060227a70a12694f1dc07424dcbd88aa64c894f
Takayuki Sei <takayuki_sei@voyagegroup.com>
2017-02-24 20:35:20 +0900

    test用のスクリプト

---
+++ Assets/Scripts/KeyboardController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardController : MonoBehaviour {
+	[SerializeField]
+	private float speed = 1;
+	
+	// Update is called once per frame
+	void Update () {
+		float move = Input.GetAxis("Horizontal") * -speed;
+		move *= Time.deltaTime;
+		transform.position = new Vector3(transform.position.x + move, transform.position.y, transform.position.z);
+	}
+}
